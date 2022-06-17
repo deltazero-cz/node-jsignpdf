9923eebd3a1c5cf537bf0c191aadb3619a0513f0
ΔO <david@deltazero.cz>
2022-06-17 15:14:55 +0200

    Peanuts

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Node Wrapper for JSignPdf
 
-Programatically add Digital Signatures (p12 certificates) to PDFs.  
+Programatically add Digital Signatures (PKCS #12 certificates, .p12 or .pdfx) to PDFs.  
 
 Based on [JSignPDF](https://github.com/intoolswetrust/jsignpdf) by [@kwart](https://github.com/kwart/)
 
@@ -8,10 +8,9 @@ Based on [JSignPDF](https://github.com/intoolswetrust/jsignpdf) by [@kwart](http
 > 
 > Project home-page: [jsignpdf.sourceforge.net](http://jsignpdf.sourceforge.net/)
 
-This node wrapper uses child_process.exec and temp files to run java app JSignPDF in non-interactive mode.
-Returns a Promise&lt;Buffer&gt;
+This node wrapper uses child_process.exec and temp files to asynchronously run java app JSignPDF in non-interactive mode.
 
-Required Java
+Requires Java.
 
 ### Quick Usage
 
@@ -21,7 +20,8 @@ import signPDF from 'jsignpdf'
 const signed : Buffer = await signPDF(
   pdf: Buffer,        // your existing PDF
   p12: Buffer,        // your signing p12 certificate 
-  passhrase: string   // passhrase to your certificate
+  passhrase: string,  // passhrase to your certificate
+  timeout: number     // process timeout, default 10.000 ms
 )
 // => Signed PDF as a Buffer
 ```
--- index.ts
+++ index.ts
@@ -3,7 +3,17 @@ import * as fs from 'fs/promises'
 import { exec } from 'child_process'
 import { resolve as resolvePath } from 'path'
 
-export default async function signPDF(pdf: Buffer, p12: Buffer, pass: string, timeout = 15000) : Promise<Buffer> {
+/**
+ * Asynchronously Sign PDF with PKCS #12 certificate
+ *
+ * @param {Buffer} pdf - existing PDF to Sign
+ * @param {Buffer} p12 - signature PKCS #12 certificate
+ * @param {string} pass - signature certificate passphrase
+ * @param {number} timeout - optional timeout, default 10.000 ms
+ * @return {Promise<Buffer>} Signed PDF
+ */
+
+export default async function signPDF(pdf: Buffer, p12: Buffer, pass: string, timeout = 10000) : Promise<Buffer> {
   const tmpdir = os.tmpdir(),
         uniq = `node-jsignpdf-${new Date().getTime()}`,
         infile = `${tmpdir}/${uniq}.pdf`,
@@ -14,7 +24,7 @@ export default async function signPDF(pdf: Buffer, p12: Buffer, pass: string, ti
   await fs.writeFile(infile, pdf)
 
   return new Promise((resolve, reject) => {
-    exec(`java -jar JSignPDF.jar -kst PKCS12 -d "${tmpdir}" -ksf "${certfile}" -ksp ${pass} "${infile}"`, {
+    exec(`java -jar JSignPDF.jar -kst PKCS12 -d "${tmpdir}" -ksf "${certfile}" -ksp "${pass}" "${infile}"`, {
       cwd: resolvePath(__dirname.replace(/\/dist$/, ''), 'lib/jsignpdf-2.2.0'),
       timeout
     }, async err => {
@@ -26,14 +36,18 @@ export default async function signPDF(pdf: Buffer, p12: Buffer, pass: string, ti
       ])
 
       if (err) return reject (err)
-      if (!data || !data.length) return reject(new Error('Result is Empty'))
+      if (!data || !data.length) return reject(new Error('Unable to Sign PDF. Result is Empty'))
       resolve(data)
     })
   }).catch(e => {
-    if (e.code === 143)
-      throw new Error('Unable to Sign. Command Failed. Timeout?')
+    if (e.code === 127)
+      throw new Error(`Unable to Sign PDF. Java Not Installed?`)
+    if (e.code === 126)
+      throw new Error(`Unable to Sign PDF. Permissions denied for [java]`)
+    if (e.killed)
+      throw new Error(`Unable to Sign PDF. Process Killed. Timeout?`)
 
     const msg = e.message.match(/java.io.IOException: (.*)\n/)?.[1]
-    throw new Error(msg || 'Unable to Sign. Incorrect Password?')
+    throw new Error(msg || 'Unable to Sign PDF. Incorrect Password?')
   }) as Promise<Buffer>
 }
